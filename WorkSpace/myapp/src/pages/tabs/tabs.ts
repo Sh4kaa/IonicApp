af06016f1573d83b7d02eb8587670dda8b2fc7b0
Sh4Kaa <darioo_dario@hotmail.com>
2018-06-29 18:12:18 -0300

    finalizando a página de intro

--- WorkSpace/myapp/src/pages/tabs/tabs.ts
+++ WorkSpace/myapp/src/pages/tabs/tabs.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
 import { FeedPage } from '../feed/feed';
-import { ContactPage } from '../contact/contact';
-import { AboutPage } from '../about/about';
+
 
 @Component({
   templateUrl: 'tabs.html'
@@ -12,8 +11,7 @@ export class TabsPage {
 
   tab1Root = HomePage;
   tab4Root = FeedPage;
-  tab3Root = ContactPage;
-  tab2Root = AboutPage;
+  
 
   constructor() {
 
