f1ac3302199447ad370b2731bc3d995ddc386c91
Sh4Kaa <darioo_dario@hotmail.com>
2018-07-16 18:20:59 -0300

    config ts provider criado

---
+++ WorkSpace/myapp/src/providers/config/config.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+
+
+/*
+  Generated class for the MoovieProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ConfigProvider {
+  
+
+  constructor() {
+    
+  }
+  
+    
+
+}
