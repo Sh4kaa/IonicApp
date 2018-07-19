6c82625e9cba851a933bda39fa452d92d11beeb1
Sh4Kaa <darioo_dario@hotmail.com>
2018-07-19 10:48:13 -0300

    slide -menu na pagina config + mudança de thema

--- WorkSpace/myapp/src/pages/configuracoes/configuracoes.ts
+++ WorkSpace/myapp/src/pages/configuracoes/configuracoes.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PerfilPage } from '../perfil/perfil';
 
 /**
  * Generated class for the ConfiguracoesPage page.
@@ -14,6 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'configuracoes.html',
 })
 export class ConfiguracoesPage {
+  rootPage = PerfilPage;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
